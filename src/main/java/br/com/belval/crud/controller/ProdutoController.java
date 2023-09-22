ab876e2a83a426dc44dc3455ff6b7a951cb371b3
Marcelo <rm92138@estudante.fieb.edu.br>
2023-09-22 20:41:50 -0300

    Adicionando readder

--- src/main/java/br/com/belval/crud/controller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -37,6 +38,25 @@ public class ProdutoController {
 	@GetMapping("/produto/list")
 	public String list(Model model) {
 		model.addAttribute("produtos", lista);
-		return "LIsta-produtos";
+		return "Lista-produtos";
+	}
+	
+	@GetMapping("/produto/{id}")
+	public String detalhe(@PathVariable int id, Model model) {
+		Produto encontrou = null;
+		for(Produto p : lista) {
+			if(p.getId() == id) {
+				//Encontrou o produto solicitado
+				encontrou = p;
+				break;
+			}
+		}
+		
+		if(encontrou != null) {
+			model.addAttribute("novoProduto", encontrou);
+			return "novo-produto-criado";
+		}
+		
+		return "produto-nao-encontrado";
 	}
 }
